4de48343c5d2a9c9cd5207c0707202228b908bb4
rmilano24 <rmilano24@gmail.com>
2018-09-06 17:52:49 -0700

    create movie listing

---
+++ src/components/Movie.vue
@@ -0,0 +1,15 @@
+<template>
+   
+        <img :src="movie.posterPath" :alt="movie.title">
+   
+</template>
+
+<script>
+    export default {
+        props: ['movie']
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/components/MoviesList.vue
+++ src/components/MoviesList.vue
@@ -1,15 +1,18 @@
 <template>
-    <div>
-        {{ movie.title }}
-    </div>
+   <ul>
+       <li v-for="movie in movies">
+           <Movie :movie="movie" />
+       </li>
+   </ul>
 </template>
 
 <script>
+    import Movie from './Movie.vue';
     export default {
         name: 'MoviesList',
         data() {
             return {
-                movie: {}
+                movies: []
             }
         },
         created: function() {
@@ -18,15 +21,18 @@
         methods: {
             fetchData: async function() {
                 try {
-                   const res = await fetch('https://api.themoviedb.org/3/movie/550?api_key=60ec72d3c6837c4e287d7292d3a025c2');
-                   const movie = await res.json();
-                   this.movie = movie;
+                   const res = await fetch('https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=60ec72d3c6837c4e287d7292d3a025c2');
+                   const movies = await res.json();
+                   this.movies = movies.results;
                 } catch (e) {
                     console.log(e)
                 }
             }
+        },
+        components: {
+            Movie
         }
-    }
+    };
 </script>
 
 <style scoped>
