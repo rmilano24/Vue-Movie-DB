e3554a3c467ba32d01e2ac275f10c858bb26ba5e
rmilano24 <rmilano24@gmail.com>
2018-09-07 09:38:32 -0700

    create routes and route params

--- src/components/Movie.vue
+++ src/components/Movie.vue
@@ -1,5 +1,7 @@
 <template>
-    <img :src="posterImage" :alt="movie.title">
+   <router-link :to="moviePath"> 
+       <img :src="posterImage" :alt="movie.title">
+   </router-link>    
 </template>
 
 <script>
@@ -9,7 +11,10 @@ const POSTER_PATH = "http://image.tmdb.org/t/p/w154";
         computed: {
             posterImage: function() {
                 return  `${POSTER_PATH}/${this.movie.poster_path}`;
-            }
+            },
+             moviePath: function() {
+                 return `/movie/${this.movie.id}`;
+             }
         }
     };
 </script>
