dcb3e1b1f122ad786e631569b65e2e9d82069bf4
rmilano24 <rmilano24@gmail.com>
2018-09-07 13:48:43 -0700

    backup header before page transitions

---
+++ src/components/Header-before-page-transitions.vue
@@ -0,0 +1,50 @@
+<template>
+    <header>
+        <h1><router-link to="/">{{ title }}</router-link></h1>
+        <transition name="fade">
+            <h1 v-if="show">Animated</h1>
+        </transition>
+        
+        <button @click="show = !show">Show/Hide</button>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: "Header",
+        props: {
+            title: String
+        },
+        data() {
+            return {
+                show: false,
+                name: 'Ryan'
+            };
+        },
+    };
+</script>
+
+<style scoped>
+header {
+    background-color: #111;
+    padding: 20px;
+    color: #fff;
+}
+h1 {
+    margin: 0px;
+}
+.fade-enter-active, .fade-leave-active {
+    transition: all 0.3s ease;
+}
+.fade-enter, .fade-leave-to {
+    opacity: 0;
+    transform: scale(0);
+}
+
+/* name-enter -> name-enter-to */
+/* name-enter-active */
+
+/* name-leave -> name-leave-to */
+/* name-leave-active */
+
+</style>
